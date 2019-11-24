cc935c9dc72857039b9e37fe9a43a3a9ebe570bc
cawfree <hello@cawfree.com>
2019-11-24 15:36:28 +0000

    Published to GitHub! 🚀

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # react-hooks-registry
-Design custom hooks that operate independently of React's builtins.
+Custom hooks for equally custom React renderers. Inspired by [Swyx](https://twitter.com/swyx)'s [Deep Dive on React Hooks](https://www.netlify.com/blog/2019/03/11/deep-dive-how-do-react-hooks-really-work/).
+
+Currently, [React Hooks](https://reactjs.org/docs/hooks-intro.html) impose a dependency on the runtime version React DOM. This makes it difficult to roll hooks-based applications in the context of [custom React renderers](https://github.com/nitin42/Making-a-custom-React-renderer). This repository aims to make it possible to work around this restriction.
+
+## 🚀 Getting Started
+
+Using [`npm`]():
+
+```bash
+npm install --save react-hooks-registry
+```
+
+Using [`yarn`]():
+
+```bash
+yarn add react-hooks-registry
+```
+
+## ✍️ Usage
+
+```bash
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HooksRegistry, { useState, useEffect } from 'react-hooks-registry';
+
+const App = ({ ...extraProps }) => {
+  const { state, setState } = useState('hello, world');
+  useEffect(
+    () => {
+      setTimeout(
+        () => setState('goodbye, world'),
+        1000,
+      );
+    },
+    [],
+  );
+  return (
+    <div className="App">
+      <code>
+        {state}
+      </code>
+    </div>
+  );
+};
+
+HooksRegistry
+  .register(
+    App,
+    document.getElementById('root'),
+    ReactDOM.render, // or use a custom renderer
+  );
+```
+
+## ✌️  License 
+[MIT](https://opensource.org/licenses/MIT)
---
+++ index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import EventEmitter from 'events';
+
+const HooksEventEmitter = new EventEmitter();
+
+// https://www.netlify.com/blog/2019/03/11/deep-dive-how-do-react-hooks-really-work/
+export const { useState, useEffect } = (function() {
+  const hooks = [];
+  let currentHook = 0;
+  HooksEventEmitter.on('render', () => (
+    currentHook = 0
+  ));
+  return {
+    useEffect(callback, depArray) {
+      const hasNoDeps = !depArray;
+      const deps = hooks[currentHook];
+      const hasChangedDeps = deps ? !depArray.every((el, i) => el === deps[i]) : true;
+      if (hasNoDeps || hasChangedDeps) {
+        hooks[currentHook] = depArray;
+        callback();
+      }
+      currentHook++;
+    },
+    useState(initialValue) {
+      hooks[currentHook] = hooks[currentHook] || initialValue;
+      const setStateHookIndex = currentHook;
+      const setState = (newState) => {
+        const result = (hooks[setStateHookIndex] = newState);
+        HooksEventEmitter.emit('requestRender');
+        return result;
+      };
+      return [hooks[currentHook++], setState];
+    },
+  }
+})();
+
+const register = (Child, toContainer, render) => {
+  class CustomHooksComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.requested = false;
+      HooksEventEmitter.on('requestRender', this.onRequestRender.bind(this));
+      HooksEventEmitter.on('render', () => this.setState({}));
+    }
+    onRequestRender() {
+      if (!this.requested) {
+        this.requested = true;
+        requestAnimationFrame(
+          () => {
+            this.requested = false;
+            HooksEventEmitter.emit('render');
+          },
+        );
+      }
+    }
+    render() {
+      return (
+        <Child
+        />
+      );
+    }
+  } 
+  return render(<CustomHooksComponent />, toContainer);
+};
+
+export default {
+  register,
+};
